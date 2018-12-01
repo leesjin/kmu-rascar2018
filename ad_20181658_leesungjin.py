c6b5f19d674b0ec48301927eca6e7de5a0a2881b
leesjin <leesjin@kookmin.ac.kr>
2018-12-01 18:19:55 +0900

    10ad project

--- ad_20181658_leesungjin.py
+++ ad_20181658_leesungjin.py
@@ -39,19 +39,35 @@ class myCar(object):
         print("start")
         countstop = 0
         while(True):
-            print(self.car.line_detector.read_digital())
+            rawData = self.car.color_getter.get_raw_data()
+            
+            print("R: ", rawData[0])
+            print("G: ", rawData[1])
+            print("B: ", rawData[2])
+            print("Clear: ", rawData[3])
+            print(type(rawData[1]))
+            
+        
+            if 400< rawData[0] < 800 and 300 > rawData[2]:
+                print("Red light")
+                self.car.accelerator.stop()
+                time.sleep(3)
+                self.car.accelerator.go_forward(40)
+                time.sleep(0.5)
+            #print(self.car.line_detector.read_digital())
             distance = self.car.distance_detector.get_distance()
-            print(distance)
+            #print(distance)
             if distance == -1:
                 continue
-            if distance <= 20:
+            if distance <= 30:
                 count += 1
             if count >= 10:
+                countstop += 1
                 p = GPIO.PWM(buzzer_pin, 100)
                 p.start(5)     # start the PWM on 5% duty cycle
                 self.car.accelerator.stop()
                 self.car.steering.turn_left(60)
-                self.car.accelerator.go_forward(50)
+                self.car.accelerator.go_forward(45)
                 p.ChangeFrequency(scale[0])
                 time.sleep(0.35)
                 p.ChangeFrequency(scale[1])
@@ -59,16 +75,12 @@ class myCar(object):
                 p.ChangeFrequency(scale[2])
                 time.sleep(0.25)
                 self.car.accelerator.stop()
-                self.car.steering.turn_right(165)
-                countstop += 1
-                self.car.accelerator.go_forward(80)
+                self.car.steering.turn_right(155)
+                self.car.accelerator.go_forward(70)
                 p.ChangeFrequency(scale[0])
                 time.sleep(0.5)
                 p.ChangeFrequency(scale[1])
                 time.sleep(1.0)
-                #p.stop()
-                #countstop += 1
-                print("count:::::::", countstop)
                 count = 0
                 while(True):
                     if(self.car.line_detector.read_digital() != [0,0,0,0,0]):
@@ -76,6 +88,13 @@ class myCar(object):
                 
             if(self.car.line_detector.read_digital() == [0,0,1,0,0]):
                 self.car.accelerator.go_forward(40)
+            elif(self.car.line_detector.read_digital() == [1,1,1,1,1]):
+                print("all")
+                if countstop == 1:
+                    self.car.accelerator.stop()
+                    break
+            elif(self.car.line_detector.read_digital() == [1,1,1,0,0]):
+                print("1101111011011")
             elif(self.car.line_detector.read_digital()[0] == 1):
                 self.car.steering.turn_left(60)
                 self.car.accelerator.go_forward(40)
@@ -93,10 +112,6 @@ class myCar(object):
                 self.car.accelerator.go_backward(40)
                 time.sleep(0.33)
                 self.car.accelerator.stop()
-            elif(self.car.line_detector.read_digital() == [1,1,1,1,1]):
-                if countstop == 4:
-                    self.car.accelerator.stop()
-                    break
             else: 
                 continue
         #self.car.accelerator.stop()
